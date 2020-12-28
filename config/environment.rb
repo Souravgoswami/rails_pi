c07956840f64e95d37806d1c83d57e6367ea1c89
Sourav Goswami <souravgoswami@protonmail.com>
2020-12-28 22:06:33 +0530

    Added pci.ids and usb.ids and updated LinuxStat to 1.1.1

--- config/environment.rb
+++ config/environment.rb
@@ -5,5 +5,11 @@ Kernel.class_exec {
 	define_method(:then) { |&block| block == self }
 } unless Kernel.respond_to?(:then)
 
+LS::PCI.hwdata_file = File.join(Rails.root, 'hwdata', 'pci.ids')
+LS::USB.hwdata_file = File.join(Rails.root, 'hwdata', 'usb.ids')
+
+LS::PCI.initialize_hwdata
+LS::USB.initialize_hwdata
+
 # Initialize the Rails application.
 Rails.application.initialize!
